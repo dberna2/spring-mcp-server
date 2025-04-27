3d85c8f3be4cafef551827f22261072e33543577
David Bernal <leo.bernal1946@gmail.com>
2025-04-27 12:51:41 +0200

    chore initial commit

---
+++ src/test/java/com/dberna2/mcpserver/springmcpserver/SpringMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dberna2.mcpserver.springmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcpServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
